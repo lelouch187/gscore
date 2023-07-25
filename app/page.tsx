4c56b69cd25e76fa5fb490ebbb98d7884c948319
lelouch187 <vanushs187@gmail.com>
2023-07-25 21:38:49 +0600

    feat(homePage): compleated css styles

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ export default function Home() {
   return (
     <div className={s.home}>
       <h1 className={s.home__title}>Get started with Gscore today!</h1>
-      <div className={`${s.cardWrapper} container`}>
+      <div className={`${s.home__cardWrapper} container`}>
         <PricingCard />
         <PricingCard />
         <PricingCard />
       </div>
+      <p className={s.home__text}>Have more than 10 sites?</p>
+      <a className={s.home__link}>Contact us</a>
     </div>
   );
 }
